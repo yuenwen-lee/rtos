3fbada2b5f1055f0d710c12021d390bc8549df3f
Wayne Lee <yuenwen.lee@gmail.com>
2020-12-16 20:51:15 -0800

    Add API for toggle green and red LED ...

--- Core/sys_device/dev_board.c
+++ Core/sys_device/dev_board.c
@@ -7,14 +7,7 @@
 
 #include <stdint.h>
 #include "kernel/sleep.h"
-#include "stm32f4xx/stm32f4xx_nucleo_144.h"
 #include "dev_board.h"
-
-
-void board_toggle_led_monitor(void)
-{
-    BSP_LED_Toggle(LED3);
-}
 
 
 void board_blink_led_cli(void)
--- Core/sys_device/dev_board.h
+++ Core/sys_device/dev_board.h
@@ -8,8 +8,25 @@
 #ifndef _DEV_BOARD_H_
 #define _DEV_BOARD_H_
 
+#include "stm32f4xx/stm32f4xx_nucleo_144.h"
 
-void board_toggle_led_monitor(void);
+
+static inline void board_toggle_led_monitor(void)
+{
+    BSP_LED_Toggle(LED3);
+}
+
+static inline void board_toggle_led_cli(void)
+{
+    BSP_LED_Toggle(LED2);
+}
+
+static inline void board_toggle_led_systck(void)
+{
+    BSP_LED_Toggle(LED1);
+}
+
+
 void board_blink_led_cli(void);
 
 
